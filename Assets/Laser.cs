3580e31908efd663c410063f76bf9d8fb44a1811
Sam <sam-davies@hotmail.co.uk>
2024-04-16 19:52:06 +0100

    removed a fair bit of debug code

--- Assets/Laser.cs
+++ Assets/Laser.cs
@@ -1,11 +1,4 @@
-using System.Collections;
-using System.Collections.Generic;
-using System.Drawing;
-using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.Rendering;
-using static UnityEngine.GraphicsBuffer;
-using static UnityEngine.ParticleSystem;
 using Color = UnityEngine.Color;
 
 public class Laser : MonoBehaviour
@@ -23,28 +16,18 @@ public class Laser : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {   
-        LaserRender.SetPosition(0, LaserStart.position);
-        
+        LaserRender.SetPosition(0, LaserStart.position);        
     }
 
     // Update is called once per frame
     void Update()
     {
-        Debug.DrawRay(LaserStart.position, transform.forward, Color.green, 5);
-        Debug.DrawRay(transform.position, transform.up, Color.blue, 5);
-        Debug.DrawRay(LaserStart.position, transform.right, Color.red, 5);
-
         if (Physics.Raycast(transform.position, transform.up, out RaycastHit hitInfo))
         {
             LaserRender.SetPosition(1, hitInfo.point);
             if (hitInfo.transform.tag == "Player")
             {
                 laserPlayerDetected();
-                print("Laser Hit player");
-            }
-            else
-            {
-                print("Laser Hit " + hitInfo.transform.name);
             }
         }
     }
